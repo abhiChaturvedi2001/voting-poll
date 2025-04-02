f44304a1ed3167e9b2e45fbbd021da0944825e62
ABHCH <chaturvedia435@gmail.com>
2025-04-02 22:52:56 +0530

    fix: change base url

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://voting-poll-omega.vercel.app",
     credentials: true
 }));
 app.use("/", authRouter);
